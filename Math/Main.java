02cee0adf53de35440dc26ed4bffefdc8a01caab
Rajesh-Waghmode <garryneon07@gmail.com>
2025-12-15 18:35:10 +0530

    explored Math Class with some useful Methods

---
+++ Math/Main.java
@@ -0,0 +1,16 @@
+
+import java.lang.Math;
+import java.math.MathContext;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(Math.PI);
+        int num;
+        System.out.println(num = (int) Math.pow(2, 10));
+        System.out.println(num);
+        
+    }
+
+}
