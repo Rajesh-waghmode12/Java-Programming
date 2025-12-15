b68bf21b7205bdfb346f669397610d747612b61b
Rajesh-Waghmode <garryneon07@gmail.com>
2025-12-15 23:10:07 +0530

    coded different defination of array like one dimensonal array multi dimensional array anonymous array

---
+++ Array/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = new int[3];
+        int array[] = new int[9];
+        String[] names = { "Rajesh ", "Avinash", "suresh" };
+
+        String[][] data = { { "Rajesh", "Suresh", "navin" }, { "ravi", "ramesh", "ganesh" } };
+
+        //anonymouse 
+        String[] list = new String[] { "Ravi", "suresh" };
+        
+        for (String ele : list) {
+            System.out.println(ele);
+        }
+
+    }
+}
